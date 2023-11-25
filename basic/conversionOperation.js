eb48c32922a309424bc52769cdbe6463e4e1236a
prathamesh123 <prathameshgolhare@gmail.com>
2023-11-25 14:08:14 +0530

    conversionOperation

---
+++ basic/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33DD"
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+
+// "22" => 22
+// "33abc"=> NaN
+// true =1; false => 0
+
+let isLoggedIn = "s"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+/*
+ 1 => ture; 0 => false
+ "" => false
+ // "hi" => true
+
+ */
+
+ let num = 33
+ let stringnum = String(num)
+ console.log(typeof stringnum);
