d51cb06cdb0aae34600d491d6311463b8198bf54
prathamesh123 <prathameshgolhare@gmail.com>
2023-11-25 13:35:57 +0530

    dataypes

--- basic/datatype.js
+++ basic/datatype.js
@@ -2,5 +2,19 @@
 
 //alert("hello")  // we are not using nodejs, not browser
 
-console.log(3+3);
-console
+let name = "prathamesh"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string =>
+// boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+//object
+
+console.log(typeof "ram");
+console.log(typeof undefined);
+console.log(typeof null);
