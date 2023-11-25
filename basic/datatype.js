9e66479930f683552df2b5c9a16ef8d92696c828
prathamesh123 <prathameshgolhare@gmail.com>
2023-11-25 13:26:45 +0530

    variable learn

---
+++ basic/datatype.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello")  // we are not using nodejs, not browser
+
+console.log(3+3);
+console
---
+++ basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = '1444'
+let accountEmail = "prathameshgolhare@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState;
+//accountId = 2
+
+console.table([accountEmail,accountId, accountPassword, accountCity, accountState])
+
+/* 
+prefer not to use var  because of issue in block scope and functional scope
+
+*/
